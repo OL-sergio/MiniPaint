8c942437fbf09e408657180fc8769a75438190cc
sergi <Qb0Gk2dRBPX>
2021-11-05 04:05:24 +0000

    Initial commit | Final Commit

---
+++ app/src/main/java/udemy/sergi/minipaint/MainActivity.kt
@@ -0,0 +1,22 @@
+package udemy.sergi.minipaint
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val myCanvasView = MyCanvasView(this)
+
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
+
+    }
+}
